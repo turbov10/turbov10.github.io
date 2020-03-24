59030afd6f3fdb58b745395468780aa276f979ba
Nick Zhong (nzhong) <nzhong@cisco.com>
2020-03-24 11:26:23 +0800

    [Initialization] The 1st build of VuePress

---
+++ deploy.sh
@@ -0,0 +1,18 @@
+# Make sure to throw error when exception caught
+set -e
+
+# build with vuepress
+npm run docs:build
+
+cd docs/.vuepress/dist
+
+git init
+git add -A
+git commit -m 'deploy'
+
+git push -f git@github.com:turbov10/turbov10.github.io.git master
+
+# If publish to some repo of "https://<USERNAME>.github.io/<REPO>"
+# git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
+
+cd -
