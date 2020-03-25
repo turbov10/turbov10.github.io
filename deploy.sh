7befa98c0d1a362aea76df0eaf3eb29736fe734c
Nick Zhong (nzhong) <nzhong@cisco.com>
2020-03-25 12:54:19 +0800

    [Initialization] Update github token

--- deploy.sh
+++ deploy.sh
@@ -6,22 +6,16 @@ npm run docs:build
 
 rm -rf tmp/
 mkdir tmp
-if [ --e docs ]; then
-   mv docs/* tmp/
-fi
-
-mv ./.gitignore tmp/
-mv ./.travis.yml tmp/
-mv ./deploy.sh tmp/
-mv ./package.json tmp/
-mv ./yarn.lock tmp/
+mv docs/* tmp/
+mv .gitignore tmp/
+mv .travis.yml tmp/
+mv deploy.sh tmp/
+mv package.json tmp/
+mv yarn.lock tmp/
 
 cp -a tmp/docs/.vuepress/dist/. ./
 
-if [ --e node_modules ]; then
-   rm -rf node_modules/
-fi
-
+rm -rf node_modules/
 rm -rf tmp/
 
 #cd docs/.vuepress/dist
